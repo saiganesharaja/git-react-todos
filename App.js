3c9d484670c4352db4e8eaa00d27a34806f93ef1
Sai Ganesh Araja <saiganesharaja@gmail.com>
2023-09-27 10:55:11 +0530

    my react programs

---
+++ App.js
@@ -0,0 +1,17 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+import "./App.css";
+import TodoApp from "./TodoApp";
+
+function App() {
+  return (
+    <div className="App">
+      <TodoApp />
+    </div>
+  );
+}
+
+export default App;
